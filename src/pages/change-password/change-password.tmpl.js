a95eb138a72b120d2b11f1b4e72d3202cd6f7595
Goliaeth <75118259+Goliaeth@users.noreply.github.com>
2022-10-12 22:53:53 +0500

    redo change-password with handlebars

---
+++ src/pages/change-password/change-password.tmpl.js
@@ -0,0 +1,10 @@
+export default `
+{{> profile-img imgPath=imgPath}}
+
+{{#each formItemList}}
+  {{> form-group }}
+{{/each}}
+
+{{> button label=btnSaveLabel className=btnSaveClass}}
+{{> button label=btnBackLabel className=btnBackClass}}
+`
