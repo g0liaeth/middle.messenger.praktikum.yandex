cdf4553fa542c7293b566059a8dfaa49ffb90444
Goliaeth <75118259+Goliaeth@users.noreply.github.com>
2022-10-20 22:48:55 +0500

    fix: fixed main index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import Handlebars from 'handlebars';
 import template from './index.tmlp';
 import './components';
 
-// eslint-disable-next-line no-undef
 window.addEventListener('DOMContentLoaded', () => {
   const compiled = Handlebars.compile(template);
-  // eslint-disable-next-line no-undef
   const root = document.getElementById('root');
 
-  root!.innerHTML = compiled({});
+  if (root !== null) {
+    root.innerHTML = compiled({});
+  }
 });
