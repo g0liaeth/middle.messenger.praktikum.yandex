6cab1100697571b08b67fb29091bc00d6a66733c
goliaeth <goliaeth@gmail.com>
2022-11-18 20:02:59 +0500

    fix: change readme

--- src/pages/ChangePassword/ChangePasswordController.ts
+++ src/pages/ChangePassword/ChangePasswordController.ts
@@ -1,13 +1,32 @@
+import AuthAPI from '../../api/AuthAPI';
 import UserAPI from '../../api/UserAPI';
 import { ChangePasswordData } from '../../types/commonTypes';
 import BaseController from '../../utils/BaseController';
 
 export default class ChangePasswordController extends BaseController {
   private _userAPI: UserAPI;
+  private _authAPI: AuthAPI;
 
   constructor() {
     super();
     this._userAPI = new UserAPI();
+    this._authAPI = new AuthAPI();
+  }
+
+  async fetchUser() {
+    try {
+      const state = this._store.getState();
+      if (!state.profileState.user.login) {
+        const res = await this._authAPI.getUserInfo();
+        if (res.status === 200) {
+          this._store.setState('profileState.user', res.data);
+        } else {
+          this._router.go('/login');
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async changePassword(formData: ChangePasswordData) {
